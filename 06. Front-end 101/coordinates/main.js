59b7c5ab1f453fe60ad5f127a3207536943c6927
youngrongoh <youngrong.oh@gmail.com>
2020-07-09 21:44:11 +0900

    06 - Tutorial: coordinates  성능 개선

--- 06. Front-end 101/coordinates/main.js
+++ 06. Front-end 101/coordinates/main.js
@@ -2,24 +2,41 @@ const horizental = document.querySelector('.horizental');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 function moveX(obj, x) {
-    obj.style.left = x;
+    obj.style.transform = `translateX(${x}px)`;
 }
 function moveY(obj, y) {
-    obj.style.top = y;
+    obj.style.transform = `translateY(${y}px)`;
 }
 function moveXY(obj, x, y) {
-    moveX(obj, x);
-    moveY(obj, y);
+    obj.style.transform = `translate(${x}px, ${y}px)`;
+}
+function coordinatesNum(obj, x, y) {
+    obj.innerHTML = `${x}px, ${y}px`
 }
 
 window.addEventListener('mousemove', (e) => {
-    const x = `${e.clientX}px`;
-    const y = `${e.clientY}px`;
+    const x = e.clientX;
+    const y = e.clientY;
     moveY(horizental, y);
     moveX(vertical, x);
-    moveXY(target, x, y);
-    moveXY(tag, x, y);
-    tag.innerHTML = `${x}, ${y}`
+    moveXY(target, x - targetHalfWidth, y - targetHalfHeight);
+    moveXY(tag, x + 20, y + 20);
+    coordinatesNum(tag, x, y);
 });
+
+const tagRect = tag.getBoundingClientRect();
+const windowHalfWidth = window.innerWidth / 2;
+const windowHalfHeight = window.innerHeight / 2;
+const tagCenterX = windowHalfWidth + tagRect.x + 20;
+const tagCenterY = windowHalfHeight + tagRect.y + 20;
+const targetCenterX = windowHalfWidth - targetHalfWidth;
+const targetCenterY = windowHalfHeight - targetHalfHeight;
+
+moveXY(tag, tagCenterX, tagCenterY);
+moveXY(target, targetCenterX, targetCenterY);
+coordinatesNum(tag, windowHalfWidth, windowHalfHeight);
