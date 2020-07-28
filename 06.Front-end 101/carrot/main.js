a10ded5a0aaf8958cf8ee51dc678132a384f7f46
youngrongoh <youngrong.oh@gmail.com>
2020-07-28 23:32:49 +0900

    06 - Tutorial: 실전: 게임 만들기 - JS: Init logic

--- 06.Front-end 101/carrot/main.js
+++ 06.Front-end 101/carrot/main.js
@@ -1,9 +1,43 @@
 'use strict'
 
 const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
 
+const CARROT_SIZE = 80;
+
+let CARROT_COUNT = 5;
 let started = false;
+
+function init() {
+    for (let i = 0; i < CARROT_COUNT; i++) {
+        const carrot = createItem('carrot');
+        const bug = createItem('bug');
+        const x1 = setRandomCoordinate().x
+        const y1 = setRandomCoordinate().y
+        const x2 = setRandomCoordinate().x
+        const y2 = setRandomCoordinate().y
+        carrot.style.transform = `translate(${x1}px, ${y1}px)`;
+        bug.style.transform = `translate(${x2}px, ${y2}px)`;
+        field.appendChild(carrot);
+        field.appendChild(bug);
+    }
+}
+
+function createItem(name) {
+    const item = document.createElement('img');
+    item.setAttribute('class', name);
+    item.setAttribute('src', `./img/${name}.png`);
+    return item;
+}
+
+function setRandomCoordinate() {
+    const x = Math.random() * (fieldRect.width - CARROT_SIZE);
+    const y = Math.random() * (fieldRect.height - CARROT_SIZE);
+    return { x: x, y: y }
+}
+
+
 
 gameBtn.addEventListener('click', _ => {
     started = !started;
@@ -11,6 +45,8 @@ gameBtn.addEventListener('click', _ => {
     if (started) {
         gameBtnIcon.classList.remove('fa-stop');
         gameBtnIcon.classList.add('fa-play');
+        field.innerHTML = '';
+        init();
     } else {
         gameBtnIcon.classList.remove('fa-play');
         gameBtnIcon.classList.add('fa-stop');
