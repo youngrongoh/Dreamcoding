5e3104281f3817de592d0a2de60ae9fbbd2b0009
youngrongoh <youngrong.oh@gmail.com>
2020-07-28 23:01:53 +0900

    06 - Tutorial: 실전: 게임 만들기 - JS: Reset and Change icon of start button

--- 06.Front-end 101/carrot/main.js
+++ 06.Front-end 101/carrot/main.js
@@ -1,27 +1,19 @@
 'use strict'
 
-// 아이템 생성 - document.createElement img태그 지정, setAttribute class 부여, setAttribute src 이미지 경로 지정 
-const carrot = document.createElement('img');
-const bug = document.createElement('img');
-carrot.setAttribute('class', 'carrot');
-carrot.setAttribute('src', './img/carrot.png');
-bug.setAttribute('class', 'bug');
-bug.setAttribute('src', './img/bug.png');
+const field = document.querySelector('.game__field');
+const gameBtn = document.querySelector('.game__button');
 
-// 랜덤한 좌표값 계산 - Math.random() max-min 필드 범위 안에서
-function getRandomCoordinate() {
-    const field = document.querySelector('.game__field');
-    const fieldRect = field.getBoundingClientRect();
-    const x = Math.random() * fieldRect.width + fieldRect.x;
-    const y = Math.random() * fieldRect.height + fieldRect.y;
-    return { x: x, y: y }
-}
+let started = false;
 
-// 아이템 좌표 - position:relative top 0, left 0 기준 위치 설정, style.transform을 이용해 좌표 부여
-
-// 필드에 아이템 배치 - appendChild
-
-// play 버튼 누르면 랜덤배치, stop으로 변경
-
-// refresh 누르면 랜덤 배치
+gameBtn.addEventListener('click', _ => {
+    started = !started;
+    const gameBtnIcon = document.querySelector('.fas');
+    if (started) {
+        gameBtnIcon.classList.remove('fa-stop');
+        gameBtnIcon.classList.add('fa-play');
+    } else {
+        gameBtnIcon.classList.remove('fa-play');
+        gameBtnIcon.classList.add('fa-stop');
+    }
+})
 
