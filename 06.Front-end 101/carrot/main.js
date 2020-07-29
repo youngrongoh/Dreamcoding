58fd8671dd9fbf8b9b6e076b65646808d6b294f6
youngrongoh <youngrong.oh@gmail.com>
2020-07-29 13:29:10 +0900

    06 - Tutorial: 실전: 게임 만들기 - JS: Init logic(lesson)

--- 06.Front-end 101/carrot/main.js
+++ 06.Front-end 101/carrot/main.js
@@ -1,40 +1,40 @@
 'use strict'
 
+const CARROT_SIZE = 80;
+const BUG_SIZE = 50;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
 
-const CARROT_SIZE = 80;
-
 let CARROT_COUNT = 5;
 let started = false;
 
-function init() {
-    for (let i = 0; i < CARROT_COUNT; i++) {
-        const carrot = createItem('carrot');
-        const bug = createItem('bug');
-        const x1 = setRandomCoordinate().x
-        const y1 = setRandomCoordinate().y
-        const x2 = setRandomCoordinate().x
-        const y2 = setRandomCoordinate().y
-        carrot.style.transform = `translate(${x1}px, ${y1}px)`;
-        bug.style.transform = `translate(${x2}px, ${y2}px)`;
-        field.appendChild(carrot);
-        field.appendChild(bug);
+function initGame() {
+    addItem('carrot', 5, 'img/carrot.png', CARROT_SIZE);
+    addItem('bug', 5, 'img/bug.png', BUG_SIZE);
+}
+
+function addItem(className, count, imgPath, padding) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - padding;
+    const y2 = fieldRect.height - padding;
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
     }
 }
 
-function createItem(name) {
-    const item = document.createElement('img');
-    item.setAttribute('class', name);
-    item.setAttribute('src', `./img/${name}.png`);
-    return item;
-}
-
-function setRandomCoordinate() {
-    const x = Math.random() * (fieldRect.width - CARROT_SIZE);
-    const y = Math.random() * (fieldRect.height - CARROT_SIZE);
-    return { x: x, y: y }
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 
@@ -43,13 +43,13 @@ gameBtn.addEventListener('click', _ => {
     started = !started;
     const gameBtnIcon = document.querySelector('.fas');
     if (started) {
-        gameBtnIcon.classList.remove('fa-stop');
-        gameBtnIcon.classList.add('fa-play');
-        field.innerHTML = '';
-        init();
-    } else {
         gameBtnIcon.classList.remove('fa-play');
         gameBtnIcon.classList.add('fa-stop');
+        field.innerHTML = '';
+        initGame();
+    } else {
+        gameBtnIcon.classList.remove('fa-stop');
+        gameBtnIcon.classList.add('fa-play');
     }
 })
 
