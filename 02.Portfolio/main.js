063918cabc5bcc1d49983399e760e6dc33532c38
youngrongoh <youngrong.oh@gmail.com>
2020-09-10 00:02:03 +0900

    JS: Scroll to section as tapping navbar menu

--- 02.Portfolio/main.js
+++ 02.Portfolio/main.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const navbar = document.querySelector('#navbar');
+const navbarMenu = navbar.querySelector('.navbar__menu');
 const navbarHeight = navbar.getBoundingClientRect().height;
+const contactBtn = document.querySelector('.home__contactBtn');
 
 // Change bg color of navbar to pink
 window.addEventListener('scroll', () => {
@@ -11,4 +13,21 @@ window.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+navbarMenu.addEventListener('click', e => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if (link === null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
+
+contactBtn.addEventListener('click', () => {
+    const contact = document.querySelector('#contact');
+    contact.scrollIntoView({ behavior: 'smooth' });
 });
