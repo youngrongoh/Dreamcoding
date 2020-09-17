7b643c91f174d8c4ab334ed1f72e9fc2beccef8b
youngrongoh <youngrong.oh@gmail.com>
2020-09-17 21:57:31 +0900

    practice 1: target coordinates

---
+++ 06.Front-end 101/chapter2/coordinates/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove', e => {
+    const x = e.clientX;
+    const y = e.clientY;
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.textContent = `${x}px, ${y}px`;
+})
