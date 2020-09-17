c8b1272b0815b95fe4a442ae20be45f39e10af7b
youngrongoh <youngrong.oh@gmail.com>
2020-09-17 14:20:00 +0900

    init

--- 05.Youtube mobile/main.js
+++
@@ -1,3 +0,0 @@
-const moreBtn = document.querySelector('#info .metadata .moreBtn');
-const title = document.querySelector('#info .metadata .title');
-moreBtn.addEventListener('click', () => title.classList.toggle('clamp'));
