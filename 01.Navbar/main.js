bb63bb3825cc378ed85101c836d7f9a09b7c6a0d
Young Rong Oh <youngrongoh@Youngui-MacBookPro.local>
2020-09-28 23:39:47 +0900

    frontend-101

--- 01.Navbar/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.nav-toggle-btn');
-const menu = document.querySelector('.nav-menu');
-const icons = document.querySelector('.nav-sns');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
