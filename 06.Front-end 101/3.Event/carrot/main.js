3a7446bda077f16e15ce0349013bbb4c8fd9b661
Young Rong Oh <youngrongoh@Youngui-MacBookPro.local>
2020-10-07 14:56:14 +0900

    Harvest Carrots: JS set game timer

--- 06.Front-end 101/3.Event/carrot/main.js
+++ 06.Front-end 101/3.Event/carrot/main.js
@@ -3,8 +3,13 @@
 const titleScreen = document.querySelector('.game__title-screen');
 const startBtn = titleScreen.querySelector('.title-screen__start');
 const playBtn = document.querySelector('.header__play');
+const timerText = document.querySelector('.header__timer');
 
-// hide title screen when start btn is clicked
+let timer;
+
+const SET_TIME_AS_SECOND = 10;
+
+// hide title screen when startBtn is clicked
 startBtn.addEventListener('click', () => {
   titleScreen.classList.add('game__title-screen--hide');
 })
@@ -24,9 +29,27 @@ playBtn.addEventListener('click', e => {
   }
 })
 
-// start timer when play btn is clicked
+// set timer
+// 1) change timer text
+function changeTimerText(time) {
+  const minutes = time / 60 > 10 ? Math.floor(time / 60) : `0${Math.floor(time / 60)}`;
+  const seconds = time % 60 > 10 ? time % 60 : `0${time % 60}`;
+  timerText.textContent = `${minutes}:${seconds}`
+}
+// 2) start timer
+function startTimer() {
+  let time = SET_TIME_AS_SECOND;
+  timer = setInterval(()=> {
+    changeTimerText(time);
+    time--;
+  }, 1000)
+}
+// 3) stop timer
+// stop timer
+function stopTimer() {
+  clearInterval(timer);
+}
 
-// start timer when replay btn is clicked
 // count down as clicking carrots
 // init game
 // put every items on field randomly
