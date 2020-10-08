514da27f869bada82573240030e88480c31a1c40
Young Rong Oh <youngrongoh@Youngui-MacBookPro.local>
2020-10-08 22:37:55 +0900

    Harvest Carrots: JS add Items Randomly

--- 06.Front-end 101/3.Event/carrot/main.js
+++ 06.Front-end 101/3.Event/carrot/main.js
@@ -4,10 +4,15 @@ const titleScreen = document.querySelector('.game__title-screen');
 const startBtn = titleScreen.querySelector('.title-screen__start');
 const playBtn = document.querySelector('.header__play');
 const timerText = document.querySelector('.header__timer');
-
-let timer;
+const field = document.querySelector('.game__field');
 
 const SET_TIME_AS_SECOND = 10;
+const CARROT_COUNT = 3;
+const BUG_COUNT = 3;
+const CARROT_SIZE = 80;
+const BUG_SIZE = 50;
+
+let timer;
 
 // hide title screen when startBtn is clicked
 startBtn.addEventListener('click', () => {
@@ -50,8 +55,35 @@ function stopTimer() {
   clearInterval(timer);
 }
 
-// count down as clicking carrots
+function createItem(className) {
+  const item = document.createElement('div');
+  item.setAttribute('class', className);
+  return item;
+}
+
+function setRandomCoordinates(item, size) {
+  const x = Math.floor(Math.random() * (field.offsetWidth - size));
+  const y = Math.floor(Math.random() * (field.offsetHeight- size));
+  item.style.left = x + 'px';
+  item.style.top = y + 'px';
+}
+
+function addItemsRandomly(className, count, itemSize) {
+  for(let i = 0; i < count; i++) {
+    const item = createItem(className);
+    setRandomCoordinates(item, itemSize);
+    field.appendChild(item);
+  }
+}
+
 // init game
+function init() {
+  field.innerHTML = '';
+  addItemsRandomly('carrot', CARROT_COUNT, CARROT_SIZE);
+  addItemsRandomly('bug', BUG_COUNT, BUG_SIZE);
+}
+
+// count down as clicking carrots
 // put every items on field randomly
 // display pop up when timer is done
 // display pop up when bugs are clicked
